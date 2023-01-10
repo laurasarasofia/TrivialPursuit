9353071adadb4ef4ca066593b45c50fd0fedddf2
laurasarasofia <laurasarasofiaa@gmail.com>
2023-01-10 23:29:51 +0200

    toimii yhden kysymyksen ajan

--- Swing.java
+++ Swing.java
@@ -10,8 +10,8 @@ import java.util.Scanner;
 public class Swing extends JFrame implements ActionListener {
 
     Scanner lukija = new Scanner(System.in);
-    Pelaaja pelaaja = new Pelaaja();
     Kysymykset kysymykset = new Kysymykset();
+    ArrayList<Pelaaja> pelaajat = new ArrayList<Pelaaja>();
     String kysymys;
     String vari;
     boolean tilanne = false;
@@ -32,7 +32,7 @@ public class Swing extends JFrame implements ActionListener {
 
         label1 = new JLabel("Kuinka monta pelaajaa? ");
         add(label1);
-        label1.setBounds(10, 10, 200, 20);
+        label1.setBounds(10, 10, 400, 20);
         label1.setForeground(Color.white);
 
         tf1 = new JTextField(10);
@@ -44,25 +44,22 @@ public class Swing extends JFrame implements ActionListener {
         button1.setBounds(10, 60, 200, 20);
         button1.addActionListener(this);
 
-       
+        button2 = new JButton("Vastaa");
+        // add(button2);
+        button2.setBounds(10, 60, 200, 20);
+        button2.addActionListener(this);
 
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
-        if (e.getSource() == button1) {
-            int maara = Integer.parseInt(tf1.getText());
-            ArrayList<Pelaaja> pelaajat = new ArrayList<Pelaaja>();
-            for (int i = 0; i < maara; i++) {
-                Pelaaja pelaaja = new Pelaaja();
-                pelaajat.add(pelaaja);
-            }
-            label1.setText(randomKysymys());
-            button1.setText("Vastaa");
 
-            /*while (tilanne == pelaajat.get(i).kaikkiVarit()) {
-                System.out.println("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
-                String vastaus1 = lukija.nextLine();
+        if (e.getActionCommand().equals("Vastaa")) {
+            while (tilanne == pelaajat.get(i).kaikkiVarit()) {
+                // set(i);
+                // label1.setText("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
+                // System.out.println("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
+                String vastaus1 = tf1.getText();
                 if (oikeaVastaus(vastaus1) == true) {
                     pelaajat.get(i).oikeaVastaus(vari);
                     System.out.println(vari + " arvattu oikein!!!");
@@ -77,13 +74,42 @@ public class Swing extends JFrame implements ActionListener {
                 } else {
                     i++;
                 }
-            }*/
+                set(i);
+
+            }
         }
-        /*if (e.getSource() == button1) {
-            label1.setText(randomKysymys());
-            button1.setText("Vastaa");
-            System.out.println("");
-        }*/
+
+        if (e.getActionCommand().equals("Aloita peli")) {
+            int maara = Integer.parseInt(tf1.getText());
+            for (int i = 0; i < maara; i++) {
+                Pelaaja pelaaja = new Pelaaja();
+                pelaajat.add(pelaaja);
+            }
+            // tf1.setText(null);
+            // button1.setText("Vastaa");
+
+            // label1.setText(randomKysymys());
+
+        }
+        // button1.setVisible(false);
+        // add(button2);
+        // button2.setVisible(true);
+        set(i);
+
+    }
+
+    /*
+     * if (e.getSource() == button1) {
+     * label1.setText(randomKysymys());
+     * button1.setText("Vastaa");
+     * System.out.println("");
+     * }
+     */
+    public void set(int i) {
+        tf1.setText(null);
+        button1.setText("Vastaa");
+        label1.setText("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
+
     }
 
     public String randomKysymys() {

ab9ac062a0ac2cbca02ca5175e7eb13eae02c28d
laurasarasofia <laurasarasofiaa@gmail.com>
2023-01-10 19:24:22 +0200

    Käyttöliittymä alkaa toimia:)

--- Swing.java
+++ Swing.java
@@ -4,14 +4,11 @@ import java.awt.event.*;
 import javax.swing.*;
 import javax.swing.event.*;
 import java.util.*;
-import java.io.*;
 import java.util.Random;
 import java.util.Scanner;
-import java.util.HashMap;
-import java.util.Map.Entry;
 
 public class Swing extends JFrame implements ActionListener {
-    // Create GUI for asking questions from user
+
     Scanner lukija = new Scanner(System.in);
     Pelaaja pelaaja = new Pelaaja();
     Kysymykset kysymykset = new Kysymykset();
@@ -19,49 +16,54 @@ public class Swing extends JFrame implements ActionListener {
     String vari;
     boolean tilanne = false;
     int i = 0;
-    private JButton button1;
+    private JButton button1, button2;
     private JLabel label1;
-    private JTextField textfield1;
-    private JLabel randomKysymys;
-    private JTextField vastaus;
-    private JButton nappi;
+    private JTextField tf1, tf2;
 
+    private JTextField vastaus;
 
     public Swing() {
 
         super("Trivial Pursuit");
-        setLayout(new FlowLayout());
-        button1 = new JButton("Vastaa");
+        setLayout(null);
+        setSize(400, 400);
+        Container c = getContentPane();
+        c.setBackground(Color.darkGray);
+
         label1 = new JLabel("Kuinka monta pelaajaa? ");
-        textfield1 = new JTextField(10);
-        randomKysymys = new JLabel(randomKysymys());
-        vastaus= new JTextField(10);
-        nappi = new JButton("Vastaa");
         add(label1);
-        add(textfield1);
+        label1.setBounds(10, 10, 200, 20);
+        label1.setForeground(Color.white);
+
+        tf1 = new JTextField(10);
+        add(tf1);
+        tf1.setBounds(10, 30, 200, 20);
+
+        button1 = new JButton("Aloita peli");
         add(button1);
-        add(randomKysymys);
-        add(vastaus);
-        add(nappi);
+        button1.setBounds(10, 60, 200, 20);
         button1.addActionListener(this);
-        nappi.addActionListener(this);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setSize(300, 100);
+
+        button2 = new JButton("Seuraava");
+        add(button2);
+        button2.setBounds(10, 90, 200, 20);
+        button2.addActionListener(this);
+
         setVisible(true);
     }
 
     public void actionPerformed(ActionEvent e) {
         if (e.getSource() == button1) {
-            int maara = Integer.parseInt(textfield1.getText());
+            int maara = Integer.parseInt(tf1.getText());
             ArrayList<Pelaaja> pelaajat = new ArrayList<Pelaaja>();
             for (int i = 0; i < maara; i++) {
                 Pelaaja pelaaja = new Pelaaja();
                 pelaajat.add(pelaaja);
             }
 
-            while (tilanne == pelaajat.get(i).kaikkiVarit()) {
-                //System.out.println("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
-                String vastaus1 = vastaus.getText();
+            /*while (tilanne == pelaajat.get(i).kaikkiVarit()) {
+                System.out.println("Vuoro: pelaaja" + (i + 1) + " " + randomKysymys());
+                String vastaus1 = lukija.nextLine();
                 if (oikeaVastaus(vastaus1) == true) {
                     pelaajat.get(i).oikeaVastaus(vari);
                     System.out.println(vari + " arvattu oikein!!!");
@@ -76,7 +78,11 @@ public class Swing extends JFrame implements ActionListener {
                 } else {
                     i++;
                 }
-            }
+            }*/
+        }
+        if (e.getActionCommand().equals("Aloita peli")) {
+            label1.setText(randomKysymys());
+            System.out.println("");
         }
     }
 
@@ -97,5 +103,3 @@ public class Swing extends JFrame implements ActionListener {
         }
     }
 }
-
-
